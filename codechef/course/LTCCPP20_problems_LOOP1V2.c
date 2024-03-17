2668c05e6387d6c7fe1f5d88a144c9824fbf9b00
Srineeja Bhowmick <srineejabhowmick@gmail.com>
2024-03-17 22:58:02 +0530

    my code

---
+++ codechef/course/LTCCPP20_problems_LOOP1V2.c
@@ -0,0 +1,19 @@
+ Write a program which does the following
+ Declare a variable a and initialise it to 0
+ Use the syntax above to create a loop, output the following to the console
+ Print a in separate lines as long as it is less than 7.
+ Increment a by 1 in each iteration.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int a = 0;
+  while (a < 7 ) {
+    cout << a << endl;
+    a = a+1;
+  }
+
+  
+}

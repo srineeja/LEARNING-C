2b8d4de57ad676a071cd159db640701089acd48e
Srineeja Bhowmick <srineejabhowmick@gmail.com>
2024-03-17 22:46:16 +0530

    my code

---
+++ codechef/course/LTCCPP02_problems_SYN4V2.c
@@ -0,0 +1,11 @@
+// Write a program which does the following
+// Add a cout statement and output the sum of 3 + 4
+// Add another cout statement and output the sum of 2 + 1.
+
+#include <stdio.h>
+
+int main() {
+    cout << 3+4 ;
+    cout << 2+1 ;
+
+}
---
+++ codechef/course/LTCCPP02_problems_SYN5V2.c
@@ -0,0 +1,11 @@
+// Write a program which does the following Output the sum of 3 and 4 using cout. Output the sum of 1 and 2 using cout, but on a new line.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << 3 + 4<< endl;
+  cout << 1 + 2;
+
+}

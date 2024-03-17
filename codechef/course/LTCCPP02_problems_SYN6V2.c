5c0e8ed46191f8a65f8028d0ea5b45994416c822
Srineeja Bhowmick <srineejabhowmick@gmail.com>
2024-03-17 22:52:37 +0530

    my code

---
+++ codechef/course/LTCCPP02_problems_SYN6V2.c
@@ -0,0 +1,8 @@
+// Let’s consider a rectangle of sides 11 and 13. Find and output area of the rectangle having sides as 11 and 13 Find and output perimeter of the rectangle having side as 11 and 13
+
+#include <stdio.h>
+
+int main() {
+    cout << 11 * 13 << endl;
+    cout << 2 * (11 + 13 );
+}
